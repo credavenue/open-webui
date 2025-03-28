b7d10f7cf63580ea47508c9dd0f00f0b5a53e1b4
uday94043 <uday.kumar@go-yubi.com>
2025-03-28 20:57:31 +0530

    remove supervisor

--- devops/docker/entrypoint.sh
+++ devops/docker/entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-echo "## :::> Starting Supervisor <:::"
-exec /usr/local/bin/supervisord -c /etc/supervisor/supervisord.conf
+#echo "## :::> Starting Supervisor <:::"
+#exec /usr/local/bin/supervisord -c /etc/supervisor/supervisord.conf
+fluentd -c /etc/fluentd/fluent.conf -v
+sleep 5
+exec bash /app/backend/start.sh
